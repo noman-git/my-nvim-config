7fde113fbfcd470e82ed4876798872c078abb15c
Noman Shahid <nomanshahid265@gmail.com>
2024-10-06 17:24:59 +0500

    Just a little formatting

--- lua/noman/lazy/telescope.lua
+++ lua/noman/lazy/telescope.lua
@@ -14,7 +14,7 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fgf', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fcw', function()
-        local word = vim.fn.expand("<cword>")
+            local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
         vim.keymap.set('n', '<leader>fcW', function()
@@ -27,4 +27,3 @@ return {
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
-
