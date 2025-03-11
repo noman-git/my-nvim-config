b95612239304f4ca291731021ec7aefd84f0216b
noman-git <nomanshahid265@gmail.com>
2025-03-11 14:05:27 +0500

    Updated trouble.lua with some more bindings

--- lua/noman/lazy/trouble.lua
+++ lua/noman/lazy/trouble.lua
@@ -17,7 +17,14 @@ return {
             vim.keymap.set("n", "]t", function()
                 require("trouble").previous({skip_groups = true, jump = true});
             end)
+            vim.keymap.set("n", "<leader>tw", function()
+                require("trouble").toggle("workspace_diagnostics") end, { desc = "Toggle Workspace Diagnostics" })
 
+            vim.keymap.set("n", "<leader>td", function()
+                require("trouble").toggle("document_diagnostics") end, { desc = "Toggle Document Diagnostics" })
+
+            vim.keymap.set("n", "<leader>tq", function()
+                require("trouble").toggle("quickfix") end, { desc = "Toggle Quickfix" })
         end
     },
 }
